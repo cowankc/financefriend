46f06f536447ecbd2247f9b278fa77a78b106245
cowankc <cowankc92@live.com>
2020-04-12 11:08:28 -0500

    added models, routes, and controllers

---
+++ models/Users.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: { type: String, required: true },
+  password: { type: String, required: true },
+  email: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Users = mongoose.model("User", userSchema);
+
+module.exports = Users;
